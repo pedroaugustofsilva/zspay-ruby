5e901fe43a977620ec5285bd7d7cdaa1d05a59b1
Pedro Augusto <pedroaugusto@hey.com>
2024-03-11 18:39:19 -0300

    Standardize Establishment class name in singular

--- lib/zspay/resources.rb
+++ lib/zspay/resources.rb
@@ -2,6 +2,6 @@
 
 require "zspay/resources/bank_account"
 require "zspay/resources/client"
-require "zspay/resources/establishments"
+require "zspay/resources/establishment"
 require "zspay/resources/sale"
 require "zspay/resources/transfer"
